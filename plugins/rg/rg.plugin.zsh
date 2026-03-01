d11e66e57121ee85ebb800fe8edabe5aef926c29
Rémino Rem <29999+remino@users.noreply.github.com>
2026-03-01 14:28:00 +0900

    Set RIPGREP_CONFIG_PATH if not set

--- plugins/rg/rg.plugin.zsh
+++ plugins/rg/rg.plugin.zsh
@@ -1,2 +1,6 @@
 alias rga="rg --follow --hidden --no-ignore"
 alias -g R='| rg'
+
+if [[ -z "$RIPGREP_CONFIG_PATH" ]]; then
+	export RIPGREP_CONFIG_PATH="${XDG_CONFIG_HOME:-$HOME/.config}/rg/config"
+fi
